2a87d6098b6a4e82c200bb964010735c9380f87f
GILM0 <ochoamacias00@gmail.com>
2025-05-13 23:47:34 -0600

    index con expressAdapter

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,14 @@
-import { createApp } from '../src/main';  // Importa tu aplicación NestJS
+import { createApp } from '../src/main';
 import serverlessExpress from '@vendia/serverless-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 let cachedServer: any;
 
 export default async function handler(req: any, res: any) {
   if (!cachedServer) {
-    const app = await createApp();
-    const expressApp = app.getHttpAdapter().getInstance();
+    const nestApp = await createApp();
+    const expressAdapter = nestApp.getHttpAdapter() as ExpressAdapter;
+    const expressApp = expressAdapter.getInstance();
     cachedServer = serverlessExpress({ app: expressApp });
   }
 
